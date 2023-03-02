c05e1c4b285eeaad5543bce4cde0817488cd709b
ipad pro <goldndolla@icloud.com>
2023-03-01 23:12:30 -0800

    Initial Commit

---
+++ xcode ipad/xcode_ipad.h
@@ -0,0 +1,18 @@
+//
+//  xcode_ipad.h
+//  xcode ipad
+//
+//  Created by Goldin Dollar on 3/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for xcode_ipad.
+FOUNDATION_EXPORT double xcode_ipadVersionNumber;
+
+//! Project version string for xcode_ipad.
+FOUNDATION_EXPORT const unsigned char xcode_ipadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <xcode_ipad/PublicHeader.h>
+
+
